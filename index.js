96a8fa4f2d33ab69b2c02b8be2b4b8b538cd37f6
Irina <myr-irina2@yandex.ru>
2023-11-29 11:22:34 +0300

    fix: footer layout mobile view

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const titles = document.querySelectorAll('.grid-col-2__title');
+const footerTitles = document.querySelectorAll('.footer__link-menu');
 
 function changeContentOnMobile() {
-  if (window.matchMedia('(max-width: 414px)').matches) {
-    titles.forEach((title, index) => {
+  const isMobile = window.matchMedia('(max-width: 414px)').matches;
+
+  titles.forEach((title, index) => {
+    if (isMobile) {
       if (index === 0) {
         title.textContent = 'Skype аудит';
       } else if (index === 1) {
-        title.textContent = '30 виджетов';
+        title.textContent = '30 виджетов';
       } else if (index === 2) {
         title.textContent = 'Dashboard';
       } else if (index === 3) {
         title.textContent = 'Месяц аmoCRM';
       }
-    });
-  } else {
-    titles.forEach((title, index) => {
+    } else {
       if (index === 0) {
         title.textContent = 'Виджеты';
       } else if (index === 1) {
@@ -22,10 +23,58 @@ function changeContentOnMobile() {
       } else if (index === 2) {
         title.textContent = 'Skype Аудит';
       } else if (index === 3) {
-        title.textContent = '35 дней';
+        title.textContent = '35 дней';
       }
-    });
-  }
+    }
+  });
+
+  footerTitles.forEach((footerTitle, index) => {
+    if (isMobile) {
+      if (index === 0) {
+        footerTitle.textContent = 'Расчёт стоимости';
+      } else if (index === 1) {
+        footerTitle.textContent = 'Услуги';
+      } else if (index === 2) {
+        footerTitle.textContent = 'Виджеты';
+      } else if (index === 3) {
+        footerTitle.textContent = 'Интеграции';
+      } else if (index === 4) {
+        footerTitle.textContent = 'Наши клиенты';
+      } else if (index === 5) {
+        footerTitle.textContent = 'Благодарность клиентов';
+      } else if (index === 6) {
+        footerTitle.textContent = 'Кейсы';
+      } else if (index === 7) {
+        footerTitle.textContent = 'Сертификаты';
+      } else if (index === 8) {
+        footerTitle.textContent = 'Блог на Youtube';
+      } else if (index === 9) {
+        footerTitle.textContent = 'Вопрос / Ответ';
+      }
+    } else {
+      if (index === 0) {
+        footerTitle.textContent = 'Расчёт стоимости';
+      } else if (index === 1) {
+        footerTitle.textContent = 'Услуги';
+      } else if (index === 2) {
+        footerTitle.textContent = 'Виджеты';
+      } else if (index === 3) {
+        footerTitle.textContent = 'Интеграции';
+      } else if (index === 4) {
+        footerTitle.textContent = 'Наши клиенты';
+      } else if (index === 5) {
+        footerTitle.textContent = 'Кейсы';
+      } else if (index === 6) {
+        footerTitle.textContent = 'Благодарственные письма';
+      } else if (index === 7) {
+        footerTitle.textContent = 'Сертификаты';
+      } else if (index === 8) {
+        footerTitle.textContent = 'Блог на Youtube';
+      } else if (index === 9) {
+        footerTitle.textContent = 'Вопрос / Ответ';
+      }
+    }
+  });
 }
 
 changeContentOnMobile();
